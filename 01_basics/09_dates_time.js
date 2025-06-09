b1b0ee3da13be16573c5779d0f2c28d0ac88cf6d
Roushni Sharma <roushni1104@gmail.com>
2025-06-09 08:01:35 +0000

    Add more JS files and update 07_strings.js

---
+++ 01_basics/09_dates_time.js
@@ -0,0 +1,18 @@
+//dates
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+
+// console.log(typeof mydate);
+
+let mycreatedDate = new Date(2025, 0, 8)  //months start from 0 in js
+console.log(mycreatedDate.toDateString());
+
+
+
+
+
